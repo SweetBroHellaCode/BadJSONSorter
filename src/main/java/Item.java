c19e144e0cb94f9db12b0e84578fd44c030cfe0c
SweetBroHellaCode <etmaron@gmail.com>
2015-10-10 17:20:05 -0400

    Finished functionality

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,28 +9,108 @@ import java.util.regex.Pattern;
  * Created by jguevara on 10/9/15.
  */
 public class Item {
-    public String name;
-    public double price;
-    public String type;
-    public Date expiration;
 
-    /**
-     *
-     * @param regex
-     * @param rawItem
-     * @return
-     */
-    private static Matcher generateMatcher(String regex, String rawItem){
-        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
-        return pattern.matcher(rawItem);
+    ArrayList<String> nameHolder = new ArrayList<String>();
+    ArrayList<String> priceHolder = new ArrayList<String>();
+    private int errorCount = 0;
+
+
+    public Item(String rawData[]) {
+
+        Pattern namePattern = Pattern.compile("[:\\w]+");
+        Pattern pricePattern = Pattern.compile("[0-9]+[.][0-9]{1,2}");
+
+        for (int i = 0; i < rawData.length; i++) {
+            Matcher nameMatcher = namePattern.matcher(rawData[i]);
+            Matcher priceMatcher = pricePattern.matcher(rawData[i]);
+
+            if (nameMatcher.find() && priceMatcher.find()) {
+                try {
+                    String[] temp = nameMatcher.group().split(":");
+                    nameHolder.add(temp[1]);
+                    priceHolder.add(priceMatcher.group());
+                } catch (Exception e) {
+                    errorCount++;
+                }
+            } else {
+                errorCount++;
+            }
+
+        }
+
+    }
+
+    public void convertToLegibleOutput() {
+
+        int appleCount = 0;
+        int milkCount = 0;
+        int breadCount = 0;
+        int cookieCount = 0;
+
+        ArrayList<String> applePrices = new ArrayList<String>();
+        ArrayList<String> milkPrices = new ArrayList<String>();
+        ArrayList<String> breadPrices = new ArrayList<String>();
+        ArrayList<String> cookiePrices = new ArrayList<String>();
+
+        Pattern applePattern = Pattern.compile("apples", Pattern.CASE_INSENSITIVE);
+        Pattern milkPattern = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
+        Pattern breadPattern = Pattern.compile("bread", Pattern.CASE_INSENSITIVE);
+
+
+        for (int i = 0; i < nameHolder.size(); i++) {
+
+            if (applePattern.matcher(nameHolder.get(i)).find()) {
+                appleCount++;
+                applePrices.add(priceHolder.get(i));
+            } else if (milkPattern.matcher(nameHolder.get(i)).find()) {
+                milkCount++;
+                milkPrices.add(priceHolder.get(i));
+            } else if (breadPattern.matcher(nameHolder.get(i)).find()) {
+                breadCount++;
+                breadPrices.add(priceHolder.get(i));
+            } else {
+                cookieCount++;
+                cookiePrices.add(priceHolder.get(i));
+            }
+
+        }
+
+        System.out.printf("%-15s %9s\n", "name:" + "\t " + "Milk", "seen: " + milkCount + " times");
+        System.out.printf("%-15s %15s\n", "=============", "=============");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + milkPrices.get(0), "seen: " + Collections.frequency(milkPrices, milkPrices.get(0)) + " times");
+        System.out.printf("%-15s %15s\n", "-------------", "-------------");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + milkPrices.get(3), "seen: " + Collections.frequency(milkPrices, milkPrices.get(3)) + " times");
+        System.out.println();
+        System.out.printf("%-17s %8s\n", "name: " + "  " + "Bread", "seen: " + breadCount + " times");
+        System.out.printf("%-15s %15s\n", "=============", "=============");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + breadPrices.get(0), "seen: " + Collections.frequency(breadPrices, breadPrices.get(0)) + " times");
+        System.out.println();
+        System.out.printf("%-17s %8s\n", "name:" + " " + "Cookies", "seen: " + cookieCount + " times");
+        System.out.printf("%-15s %15s\n", "=============", "=============");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + cookiePrices.get(0), "seen: " + Collections.frequency(cookiePrices, cookiePrices.get(0)) + " times");
+        System.out.println();
+        System.out.printf("%-17s %8s\n", "name: " + " " + "Apples", "seen: " + appleCount + " times");
+        System.out.printf("%-15s %15s\n", "=============", "=============");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + applePrices.get(0), "seen: " + Collections.frequency(applePrices, applePrices.get(0)) + " times");
+        System.out.printf("%-15s %15s\n", "-------------", "-------------");
+        System.out.printf("%-16s %9s\n", "Price:" + "\t " + applePrices.get(1), "seen: " + Collections.frequency(applePrices, applePrices.get(1)) + " times");
+        System.out.println();
+        System.out.printf("%-22s %8s\n", "Errors: ", errorCount + " times");
+
+
+
+
     }
 
 
-    public Item(String input){
-        String[] itemArray = input.split("[;^*%@!]");
-
-        Matcher name = generateMatcher("name:([\\w\\d]+)", itemArray[0]);
-        name.find();
-        System.out.println(name.group(1));
-    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/ItemParser.java
+++ src/main/java/ItemParser.java
@@ -1,48 +1,39 @@
 import java.util.ArrayList;
 
 public class ItemParser {
-    private String input = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##"+
-                           "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##"+
-                           "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##"+
-                            "naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##"+
-                            "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##"+
-                            "naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##"+
-                            "naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##"+
-                            "naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##"+
-                            "naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##"+
-                            "naMe:;price:3.23;type:Food^expiration:1/04/2016##";
 
-    private String input2 = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
+    private String input = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##" +
+            "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##" +
+            "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##" +
+            "naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##" +
+            "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##" +
+            "naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##" +
+            "naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##" +
+            "naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##" +
+            "naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##" +
+            "naMe:;price:3.23;type:Food^expiration:1/04/2016##";
 
-    /**
-     * 
-     * @return
-     */
-    public String[] parseInput(){
+   // private String input2 = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
+
+
+    public String[] parseInput() {
         String[] parsed = input.split("##");
-        for(int i=0; i<parsed.length; i++){
-            System.out.println(parsed[i]);
-        }
-
         return parsed;
-
     }
 
-    /**
-     *
-     * @param _input
-     */
-    public ItemParser(String _input){
+    public ItemParser(String _input) {
         this.input = _input;
     }
-    public ItemParser(){
+
+    public ItemParser() {
 
     }
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ItemParser itemParser = new ItemParser();
-        itemParser.parseInput();
-        Item itemTest = new Item(itemParser.parseInput()[0]);
+
+        Item itemList = new Item(itemParser.parseInput());
+
+        itemList.convertToLegibleOutput();
     }
 }
